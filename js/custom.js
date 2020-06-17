90d8c5e4caed7fe3646a2ad27aac213abc16320a
gerson-dev <gersondev@hotmail.com>
2020-06-17 15:53:38 -0300

    Arquivos da home

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+// iniciadores do carroussel
+
+
+
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        duration: 500,
+        dist:-200,
+        shift: 100,
+        padding: 300,
+        numVisible: 5
+        
+    });
+
+    $('.prev').click(function(){
+        $('.carousel').carousel('prev');
+    })
+
+    $('.next').click(function(){
+        $('.carousel').carousel('next');
+    })
+    
+
+  });
+
+// aciona o menu mobile
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+//   aciona o menu dropdown
+
+$(".dropdown-trigger").dropdown({
+    coverTrigger: false,
+    hover:true
+  });
+
+//   aciona o slider-banner
+$(document).ready(function(){
+    $('.slider').slider({
+        interval: 7000,
+        duration: 3000,
+        height: 450,
+        indicators: false
+    });
+  });
